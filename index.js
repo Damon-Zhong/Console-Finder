49675a3327ef1a4654388b5ba8382d2c1588a02b
Yuqi Zhong <damon.yq.zhong@gmail.com>
2020-06-04 14:48:56 -0400

    Update javascript functions

--- index.js
+++ index.js
@@ -1,104 +1,109 @@
-// const api = '';
+const carouselEle= document.querySelector('#carouselExampleControls');
+const displayEl = document.querySelector('#display');
+const productDisEl = document.querySelector("#productDis")
+const productList = [
+    {'Name':'PS4', 'Pic':'https://i5.walmartimages.ca/images/Large/766/752/6000197766752.jpg', 'Description': 'sometext','AmazonPrice':'$500','GooglePrice':'$498'},
+    {'Name':'Switch', 'Pic':'https://www.nintendo.com/content/dam/noa/en_US/hardware/switch/nintendo-switch-new-package/gallery/package_redblue.jpg', 'Description': 'sometext','AmazonPrice':'$450','GooglePrice':'$498'},
+    {'Name':'Xbox', 'Pic':'https://images-na.ssl-images-amazon.com/images/I/61p7mgi0GAL._AC_SL1200_.jpg', 'Description': 'sometext','AmazonPrice':'$500','GooglePrice':'$498'}]
 
-// async function game(api){
 
-//     const result = await fetch(api).then((result)=>result.json());
-//     console.log(result);
+function getConsoleName(){
+    let inputName = document.querySelector("#searchBox").value;
+    console.log(inputName)
+    return inputName
+}
+
+function searchConsole( consoleName ){
+    //Hide slideshow and productlist 
+    carouselEle.style.display = 'none';
+    productDisEl.style.display = 'none';
+    //Match input with our product list
+    for( let i=0; i<productList.length; i++){
+        if( productList[i].Name.includes( consoleName )){
+            //Display product based on consoleName
+            displayEl.innerHTML = 
+            `<div id=${productList[i].Name} class="jumbotron">
+                <p>${productList[i].Name}</p>
+                <img src="${productList[i].Pic}" alt="">
+                <p class="lead">${productList[i].Description}</p>
+                <hr class="my-4">
+                <p>Prices</p>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">Amazon Price: ${productList[i].AmazonPrice}</span>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">Google Shopping Price: ${productList[i].GooglePrice}</span>
+                    </div>
+                </div>
+            </div>`;
+            break;
+        }else{
+            displayEl.innerHTML = `<h3>Sorry! We could not find your console.`;
+        }
+    }
+}
     
-// }
+async function fetchAmazonData( consoleName ){
+    let AmazonAPI = `https://amazon-price1.p.rapidapi.com/search?keywords=${consoleName}&marketplace=CA`
+    const AmazonData = await fetch(AmazonAPI, {"method": "GET","headers": {
+		"x-rapidapi-host": "amazon-price1.p.rapidapi.com",
+		"x-rapidapi-key": ""
+	    }
+     }).then((result)=>result.json()).catch(err => {console.log(err);});
+     return AmazonData;
+}
+
+async function fetchGoogleData(){
+    let googleAPI = ''
+    const AmazonData = await fetch( googleAPI ).then( result => result.json())
+}
 
 
-//user input console name click search
-//slideshow section replaced by product card with price in CA$ (option: convert currency)
 
-//user click compare
-//display data from api (call api function)
+// var searchConsoleBtn= document.querySelector('#searchBtn');
+// searchConsoleBtn.addEventListener('click', searchConsoleFun);
 
-//user click covert currency
-//calculate converted amount in designated currency
-//search by input return
+// var productEle= document.querySelector('#allProducts');
 
-
-// const searchEl = document.querySelector("#searchBox")
-// const carouselEl = document.querySelector("#carousel")
-// const displayEl = document.querySelector("#display")
-
-// const productList = [
-//     {'Name':'PS4', 'pic':'https://i5.walmartimages.ca/images/Large/766/752/6000197766752.jpg','Description':'sometext'},
-//     {'Name':'PS3', 'pic':'https://i5.walmartimages.ca/images/Large/766/752/6000197766752.jpg','Description':'sometext'},
-//     {'Name':'PS2', 'pic':'https://i5.walmartimages.ca/images/Large/766/752/6000197766752.jpg','Description':'sometext'},
-// ]
-
-// function searchConsole( consoleName ){
-//     //Hide the slideshow
-//     carouselEl.style.display = 'none';
-//     productList.forEach( 
-//         function( item, index ){
-//             if( item.Name.includes( consoleName )){
-//                 console.log(`display`)
-//                 displayEl.innerHTML = `
-//                 <div class="card">
-//                     <img src="${item.pic}" class="card-img-top"/>
-//                     <div class="card-body">
-//                         <h5 class="card-title">${item.Name}</h5>
-//                         <p class="card-text"> ${item.Description} </p>
-//                         <button href="#" target="_blank" class="btn btn-primary" onclick="comparePrices(${item.Name})">Compare</button>
-//                     </div>
-//                 </div>`;
-//             } 
-//         }
-//     )
-// }
-
-// function comparePrices(){
-
-// }
-var searchConsoleBtn= document.querySelector('#searchBtn');
-searchConsoleBtn.addEventListener('click', searchConsoleFun);
-var carouselEle= document.querySelector('.carousel');
-var productEle= document.querySelector('#allProducts');
-
-function searchConsoleFun(){
-    console.log('works');
-    carouselEle.innerHTML = '<div id="carouselExampleControls" class="carousel slide hide" data-ride="carousel"></div>';
-    console.log(carouselEle.innerHTML); 
-    productEle.classList.remove('hideProduct');
-
-
+// function searchConsoleFun(){
     
+//     carouselEle.innerHTML = '<div id="carouselExampleControls" class="carousel slide hide" data-ride="carousel"></div>';
+//     console.log(carouselEle.innerHTML); 
+//     productEle.classList.remove('hideProduct');
+// }
+// var productPS4Ele=document.querySelector('#productPS4');
+// var compareBtnPS4Btn=document.querySelector('#compareBtnPS4');
+// console.log(document.querySelector('#compareBtnPS4'))
+// compareBtnPS4Btn.addEventListener('click', compareBtnPS4Fun);
+// function compareBtnPS4Fun(){
+//     event.preventDefault;
+//     console.log('compareBtnPS4 works');
+//     productEle.innerHTML='<div id="allProducts" class="row hideProduct hide">'
+//     productPS4Ele.classList.remove('hidePS4');
+// }
 
+// var productSwitchEle=document.querySelector('#productSwitch');
+// var compareBtnSwitchBtn=document.querySelector('#compareBtnSwitch');
+// compareBtnSwitchBtn.addEventListener('click',compareBtnSwitchFun);
+// function compareBtnSwitchFun(){
+//     event.preventDefault;
+//     console.log('compareBtnSwitch works');
+//     productEle.innerHTML='<div id="allProducts" class="row hideProduct hide">';
+//     productSwitchEle.classList.remove('hideSwitch');
+// }
 
-}
-var productPS4Ele=document.querySelector('#productPS4');
-var compareBtnPS4Btn=document.querySelector('#compareBtnPS4');
-compareBtnPS4Btn.addEventListener('click', compareBtnPS4Fun);
-function compareBtnPS4Fun(){
-    event.preventDefault;
-    console.log('compareBtnPS4 works');
-    productEle.innerHTML='<div id="allProducts" class="row hideProduct hide">'
-    productPS4Ele.classList.remove('hidePS4');
-
-}
-
-var productSwitchEle=document.querySelector('#productSwitch');
-var compareBtnSwitchBtn=document.querySelector('#compareBtnSwitch');
-compareBtnSwitchBtn.addEventListener('click',compareBtnSwitchFun);
-function compareBtnSwitchFun(){
-    event.preventDefault;
-    console.log('compareBtnSwitch works');
-    productEle.innerHTML='<div id="allProducts" class="row hideProduct hide">';
-    productSwitchEle.classList.remove('hideSwitch');
-}
-
-var productXboxEle=document.querySelector('#productXbox');
-var compareBtnXboxBtn= document.querySelector('#compareBtnXbox');
-compareBtnXboxBtn.addEventListener('click',compareBtnXboxFun);
-function compareBtnXboxFun(){
-    event.preventDefault;
-    console.log('compareBtnXbox works');
-    productEle.innerHTML='<div id="allProducts" class="row hideProduct hide">';
-    productXboxEle.classList.remove('hideXbox')
-}
+// var productXboxEle=document.querySelector('#productXbox');
+// var compareBtnXboxBtn= document.querySelector('#compareBtnXbox');
+// compareBtnXboxBtn.addEventListener('click',compareBtnXboxFun);
+// function compareBtnXboxFun(){
+//     event.preventDefault;
+//     console.log('compareBtnXbox works');
+//     productEle.innerHTML='<div id="allProducts" class="row hideProduct hide">';
+//     productXboxEle.classList.remove('hideXbox')
+// }
 
 // var homeBtnEle=document.querySelector('#homeBtn');
 // homeBtnEle.addEventListener('click', homeBtnFun);
